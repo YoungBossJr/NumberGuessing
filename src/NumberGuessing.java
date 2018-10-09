bc120690b7a2134ec72f038f165c94378faf32f2
Unknown <steven.washington.2021@student.lhschools.org>
2018-10-09 14:13:45 -0400

    Updated changes to Number Guessing Game

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -3,48 +3,48 @@ import java.util.Scanner;
 public class NumberGuessing {
 
 
-    public static void main(String[] args) {
+    public static void main(String[]args){
 
         Scanner keyboard;
-        int GuessedNumber;
-        int PickedNumber;
+        int pickedNumber;
+        int guessedNumber;
         boolean OnPoint;
 
         keyboard = new Scanner(System.in);
-        PickedNumber = (int)(Math.random()*100);
+        pickedNumber = (int)(Math.random()*100);
         OnPoint = false;
 
 
-        System.out.println("Hello there, and WELCOME to the Number Guessing Game!");
-        System.out.println("The rules couldn't be easier.");
-        System.out.println("Guess a random Number.");
-        System.out.println("If the number is too high, then guess another number.");
-        System.out.println("If the number is too low, guess again");
-        System.out.println("Keep guessing until you get the right number.");
-        GuessedNumber = keyboard.nextInt();
+        System.out.println("Hello there and WELCOME to the Number Guessing Game.");
+        System.out.println("The rules are simple.");
+        System.out.println("Guess a number if, it is too high guess another number.");
+        System.out.println("If the number is too high, guess another number.");
+        System.out.println("Keep guessing a number until you get it right.");
+        guessedNumber = keyboard.nextInt();
 
-        while (OnPoint){
+        while (!OnPoint){
 
-            if (GuessedNumber < PickedNumber){
+            if (guessedNumber < pickedNumber){
                 System.out.println("Your number is too low.");
 
 
-            }else if (GuessedNumber > PickedNumber){
+            }  else if (guessedNumber > pickedNumber){
                 System.out.println("Your number is too high.");
 
-            }else if (GuessedNumber == PickedNumber){
-                System.out.println("That is the correct number.");
+            }  else if (guessedNumber == pickedNumber){
+                System.out.println("You got it right.");
 
 
                 OnPoint = true;
                 break;
-            }
+
+                }
 
             System.out.println("Guess Again.");
-            GuessedNumber = keyboard.nextInt();
+            guessedNumber = keyboard.nextInt();
 
         }
-        System.out.println("Thank You for playing the Number Guessing Game!!!");
+        System.out.println("Thank you for playing the Number Guessing Game!!!");
 
     }
 
